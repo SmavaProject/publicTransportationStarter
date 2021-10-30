bfcee44b845b8a626f13494b5513be50f9084636
SmavaProject <katyakravchenka@yahoo.com>
2021-10-29 17:23:34 -0700

    turnstile

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -14,12 +14,9 @@ logger = logging.getLogger(__name__)
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
 
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -31,18 +28,12 @@ class Turnstile(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
         super().__init__(
             f"{station_name}", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=1,
+            num_replicas=1,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -56,4 +47,8 @@ class Turnstile(Producer):
         # TODO: Complete this function by emitting a message to the turnstile topic for the number
         # of entries that were calculated
         #
-        #
+        self.producer.produce( # <---??????
+            topic=self.station_name,
+            key={"timestamp": timestamp},
+            value={"num_entries": num_entries}
+        )
